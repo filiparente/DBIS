7356ae84680bc80dd2a8e3c3c3ed5be04ffa0e74
Unknown <mouradiogo@hotmail.com>
2018-12-03 18:07:54 +0000

    stored procedure added, triggers repositioned

--- database_schema.sql
+++ database_schema.sql
@@ -34,37 +34,7 @@ create table phone_number (
     foreign key (VAT) references person(VAT) on delete cascade
 );
 
---TRIGGER 1
-drop trigger if exists new_consult_birthday;
-delimiter $$
-create trigger new_consult_birthday before insert on consult  
-for each row
-begin
 
-  update animal a set a.age= YEAR(new.date_timestamp)-(a.birth_year-1) where a.name = new.name;
-end$$ 
-delimiter ;
-
---TRIGGERS 2
-drop trigger if exists vetdoc_assist_check; 
-DELIMITER $$ 
-create trigger vetdoc_assist_check before insert on assistant 
-for each row 
-begin 
-if new.VAT in (select VAT from assistant) then signal sqlstate '45000' set message_text = "Sorry, there's already an assistant registered in this hospital with the same information"; 
-end if; 
-end$$ 
-DELIMITER ; 
-
-drop trigger if exists assist_vetdoc_check; 
-DELIMITER $$ 
-create trigger assist_vetdoc_check before insert on veterinary 
-for each row 
-begin 
-if new.VAT in (select VAT from veterinary) then signal sqlstate '45000' set message_text = "Sorry, there's already a Veterinary Doctor registered in this hospital with the same information"; 
-end if; 
-end$$ 
-DELIMITER ;
 
 -- TRIGGER 3 OPTION 1
 drop trigger if exists phone_number_check;
@@ -252,3 +222,56 @@ create table produced_indicator (
     foreign key(name,VAT_owner,date_timestamp,num) references procedures(name,VAT_owner,date_timestamp,num) on delete cascade,
     foreign key(indicator_name) references indicator(name)
 );
+
+--TRIGGERS AND PROCEDURE--
+
+
+--TRIGGER 1
+drop trigger if exists new_consult_birthday;
+delimiter $$
+create trigger new_consult_birthday before insert on consult  
+for each row
+begin
+
+  update animal a set a.age= YEAR(new.date_timestamp)-(a.birth_year-1) where a.name = new.name;
+end$$ 
+delimiter ;
+
+--TRIGGERS 2
+drop trigger if exists vetdoc_assist_check; 
+DELIMITER $$ 
+create trigger vetdoc_assist_check before insert on assistant 
+for each row 
+begin 
+if new.VAT in (select VAT from assistant) then signal sqlstate '45000' set message_text = "Sorry, there's already an assistant registered in this hospital with the same information"; 
+end if; 
+end$$ 
+DELIMITER ; 
+
+drop trigger if exists assist_vetdoc_check; 
+DELIMITER $$ 
+create trigger assist_vetdoc_check before insert on veterinary 
+for each row 
+begin 
+if new.VAT in (select VAT from veterinary) then signal sqlstate '45000' set message_text = "Sorry, there's already a Veterinary Doctor registered in this hospital with the same information"; 
+end if; 
+end$$ 
+DELIMITER ;
+
+--STORED PROCEDURE
+
+drop PROCEDURE if exists refval_mili2centi;
+DELIMITER $$ 
+create PROCEDURE refval_mili2centi ()
+
+begin
+  
+  update indicator 
+  set reference_value= reference_value/10 where indicator.unit="miligrams";
+  update produced_indicator
+  set produced_indicator.value=produced_indicator.value/10 where produced_indicator.indicator_name in (select i.name from indicator i where i.unit="miligrams");
+
+end$$ 
+DELIMITER ;
+
+
